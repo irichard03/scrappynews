21a42ea96263308769c559db1ed27220ab06c97e
Ian Richard <ianrichard03@gmail.com>
2018-12-23 12:45:03 -0600

    switched to scrape houston chron because Times can go suck an egg.

--- controllers/scrappycontroller.js
+++ controllers/scrappycontroller.js
@@ -2,16 +2,29 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
+const articleArray = [];
 
 //TODO import model here.
 
-router.get("/scraper", async (req,res) => {
-    console.log("route hit.");
+router.get("/scraper", function (req,res) {
+   
+    axios.get("https://www.chron.com/news/houston-texas/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    const $ = cheerio.load(response.data);
+    $("a").each(function(i, element){
+       let result = $(this).children().text();
+       if(result){
+        console.log(result);
+       }
+    });
+});
+   
+
+
 });
 
-
 router.get("/", async (req,res) => {
-    res.render("index");
+    await res.render("index");
 });
 
 
